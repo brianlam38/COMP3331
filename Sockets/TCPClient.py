df219c798399404b70c06f8329b250383c6ad692
brianlam38 <brian.lam38@gmail.com>
2017-08-09 00:02:43 +1000

    adding client socket program notes

--- Sockets/TCPClient.py
+++ Sockets/TCPClient.py
@@ -3,12 +3,16 @@ total = len(sys.argv)
 cmdargs = str(sys.argv)
 
 from socket import *
+
 serverName = str(sys.argv[1]);
-serverPort = 12000 #change this port number if required
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
-sentence = raw_input('Input lowercase sentence:')
-clientSocket.send(sentence)
-modifiedSentence = clientSocket.recv(1024)
+serverPort = 12000 #change port number if required
+clientSocket = socket(AF_INET, SOCK_STREAM)			# create socket for server
+														# AF_INET = ip address family (e.g. Internet Protocol v4 addresses)
+														# SOCK_STREAM = TCP socket (SOCK_DGRAM = UDP socket)
+clientSocket.connect((serverName, serverPort))		# initiates a TCP server connection
+
+sentence = raw_input('Input lowercase sentence:')	# grab client input
+clientSocket.send(sentence)							# transmits data to server
+modifiedSentence = clientSocket.recv(1024)			# receives data from server
 print 'From Server:', modifiedSentence
-clientSocket.close()
+clientSocket.close()								# closes the socket
--- Sockets/TCPServer.py
+++ Sockets/TCPServer.py
@@ -1,12 +1,12 @@
 from socket import *
-serverPort = 12000 # change this port number if required
-serverSocket = socket(AF_INET, SOCK_STREAM)
+serverPort = 12000 # change port number if required
+serverSocket = socket(AF_INET, SOCK_STREAM)				# create socket for client
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 print "The server is ready to receive"
 while 1:
     connectionSocket, addr = serverSocket.accept()
-    sentence = connectionSocket.recv(1024)
+    sentence = connectionSocket.recv(1024)				# receive TCP message
     capitalizedSentence = sentence.upper()
     connectionSocket.send(capitalizedSentence)
     connectionSocket.close()
