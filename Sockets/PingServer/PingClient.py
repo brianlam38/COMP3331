910843133a88aa3b239a41e1e3ea853766e078ff
brianlam38 <brian.lam38@gmail.com>
2017-08-10 22:29:22 +1000

    updating ping client program with timestamp in request

--- Sockets/PingServer/PingClient.py
+++ Sockets/PingServer/PingClient.py
@@ -20,19 +20,19 @@ while seq_num < 10:
 	# transmit data to server, receive from server
 	try:
 		send_time = time.time()
-		ping = 'Ping to ' + server_name + ', seq = ' + str(seq_num)
+		ping = 'Ping to ' + server_name + ', seq = ' + str(seq_num) + str(send_time)
 		client_socket.sendto(ping, (server_name, server_port))
 		reply, server_address = client_socket.recvfrom(2048)
+		recv_time = time.time()
 		time.sleep(1)
 	# timeout exception
 	except timeout:
 		print 'No response from server, packet dropped'
 		seq_num += 1
 		continue
-	# calculate RTT, print server response
-	recv_time = time.time()
-	RTT = round(recv_time - send_time, 3)
-	print 'From server: ', reply + ', rtt = ' + str(RTT)
+	# calculate RTT, print server response with RTT
+	RTT = round(recv_time - send_time, 3) * 1000
+	print reply[0:26] + ', rtt =', RTT, 'ms'
 	seq_num += 1
 
 client_socket.close()
