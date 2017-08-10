afb21f67433733240b8dafa6936509331bfbee60
brianlam38 <brian.lam38@gmail.com>
2017-08-10 22:09:37 +1000

    updating ping client program

--- Sockets/PingServer/PingClient.py
+++ Sockets/PingServer/PingClient.py
@@ -13,19 +13,26 @@ import time
 server_name = '127.0.0.1'
 server_port = 12000 # change port number if required
 client_socket = socket(AF_INET, SOCK_DGRAM)
+client_socket.settimeout(1)
 
 seq_num = 0
 while seq_num < 10:
-	send_time = time.time()									# transmit time
-	ping = 'Ping to ' + server_name + ', seq = ' + str(seq_num) + ', '  + str(send_time)
-	client_socket.sendto(ping, (server_name, server_port))	# transmit data to server
-	time.sleep(1)											# wait 1
-										
-	reply, server_address = client_socket.recvfrom(2048)	# receive data from server
-	recv_time = time.time()									# receive time
-
-	RTT = round(recv_time - send_time, 3)					# calculate RTT
-	print 'From server: ', reply
+	# transmit data to server, receive from server
+	try:
+		send_time = time.time()
+		ping = 'Ping to ' + server_name + ', seq = ' + str(seq_num)
+		client_socket.sendto(ping, (server_name, server_port))
+		reply, server_address = client_socket.recvfrom(2048)
+		time.sleep(1)
+	# timeout exception
+	except timeout:
+		print 'No response from server, packet dropped'
+		seq_num += 1
+		continue
+	# calculate RTT, print server response
+	recv_time = time.time()
+	RTT = round(recv_time - send_time, 3)
+	print 'From server: ', reply + ', rtt = ' + str(RTT)
 	seq_num += 1
 
 client_socket.close()
