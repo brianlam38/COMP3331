5da0ed185842d8b392bf29cbe689d883803028f1
brianlam38 <brian.lam38@gmail.com>
2017-08-16 20:55:35 +1000

    adding web server code

--- 2.WebServer/WebServer.py
+++ 2.WebServer/WebServer.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 # SIMPLE WEB SERVER
 #
 # Author: Brian Lam
@@ -12,5 +14,25 @@
 # 5. Create a HTTP response message consisting of the requested file preceded by header lines
 # 6. Send the response over the TCP connection to the requesting browser
 # 7. If the requested file is not present on the server, send a HTTP "404 not found" message to client
+
+from socket import *
+
+serverPort = 12000
+serverSocket = socket(AF_INET, SOCK_STREAM)		# create socket for client
+serverSocket.bind(('', serverPort))				# binds address (hostname, port numer) to socket
+serverSocoket.listen(1)							# Listen / wait for connections made to the socket
+print "The server is ready to receive\n"		#		Arg 1 = max no. queued connections
+
+while 1:
+	connectionSocket, addr = serverSocket.accept()		# passively accepts TCP client connection
+    														# Waiting until connection arrives
+    														# New socket created on return
+    sentence = connectionSocket.recv(1024)				# receives data from client
+    capitalizedSentence = sentence.upper()				# manipulates data
+    connectionSocket.send(capitalizedSentence)			# transmits data to client
+    connectionSocket.close()							# close the socket
+
+
+
 
 
