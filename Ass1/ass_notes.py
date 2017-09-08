72ee661d2c5160746aad12ef633aa76e5faa1cda
brianlam38 <brian.lam38@gmail.com>
2017-09-08 16:20:26 +1000

    updating ass notes

--- Ass1/ass_notes.py
+++ Ass1/ass_notes.py
@@ -1,5 +1,8 @@
 # Reliable Data Transfer
-IP does not guarantee:
--> datagram delivery
--> in-order delivery
--> 
+IP does not guarantee: datagram delivery | datagram in-order delivery | datagram integrity
+
+'NOTE: Datagrams'
+-> Dont care whether data is reached to its dest or not, nor errors occurred during transmission.
+-> UDP uses Datagrams
+
+
