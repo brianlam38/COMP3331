69a39a9e5e1eb53877872d9c967932f881e29e63
brianlam38 <brian.lam38@gmail.com>
2017-09-08 17:29:02 +1000

    updating spec notes

--- Ass1/ass_notes.py
+++ Ass1/ass_notes.py
@@ -1,4 +1,5 @@
 # Reliable Data Transfer
+
 IP does not guarantee:
 -> datagram delivery | datagram in-order delivery | datagram integrity
 With IP, datagrams can:
@@ -12,11 +13,17 @@ TCP creates reliable data transfer on top of IP.
 -> While TCP is great in theory, timer management can require considerable overhead.
 -> Recommended TCP timer management system = use only a single transmission tier
 
+# Three major events to TCP data transmission
+
 Three major events to data transmission and re-transmission in the TCP sender.
 1. Data received from the app above
 2. Timer timout
 3. ACK receipt
-On the 1st event, TCP receives data from app -> encapsulates data in a segment -> passes seg to IP
+
+'FIRST EVENT: TCP receives data'
+On occurrence of event:
+-> TCP encapsulates data in a segment
+-> TCP passes seg to IP
 Each segment includes a sequence number that is the byte-stream number of the 1st data byte in
 the segment.
 
@@ -25,7 +32,26 @@ is passed to IP (The timer is associated w/ the oldest unacknowledged segment)
 
 Expiration interval for the timer = TimeoutInterval, calculated from EstimatedETT and DEVRTT.
 
-' REFER TO SIMPLIFIED TCP SENDER C PROGRAM IN DIR '
+--- REFER TO SIMPLIFIED TCP SENDER C PROGRAM IN DIR ---
+
+'SECOND EVENT: Timer timeout'
+On occurrence of event:
+-> TCP responds to timout by retransmitting the segment that caused timeout
+-> TCP then restarts the timer
+
+'THIRD EVENT: Arrival of an Acknowledgement segment ACK from the RECEIVER'
+On occurence of event:
+-> TCP compares ACK value y with variable SendBase.
+   TCP state var SendBase = seq no. of the oldest unacknowledged byte.
+   SendBase-1 = seq no. of the last byte that is known to have been received correctly and in-order at receiver.
+-> TCP uses cumulative acknowledgements, so 'y' acknowledges receipt of all bytes before byte number 'y'
+   If 'y > SendBase' then ACK is acknowledging one or more previously unacknowledged segments.
+   Thus, sender updates its 'SendBase' var.
+   It also restarts the timer if there currently arent any not yet acknowledged segments
+
+
+
+
 
 
 
