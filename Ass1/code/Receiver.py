9fd93cb273dbe553128c6837d68f6a5e3faaf9a1
brianlam38 <brian.lam38@gmail.com>
2017-09-10 17:59:17 +1000

    finishing data transfer between sender and receiver

--- Ass1/code/Receiver.py
+++ Ass1/code/Receiver.py
@@ -17,6 +17,7 @@
 # (seq 21)
 
 import pickle
+import sys
 from socket import *
 
 class STPPacket:
@@ -33,34 +34,61 @@ class Receiver:
 	# receiver needs to have a sequence number so it can deal with out-of-order pakes
 	# receiver receives stp_packet and then writes the data inside the packet
 
-	port = 4096
+	# initialise sender data: seq number, timeout
+	def __init__(self, port, file):
+		self.port = int(port)
+		self.file = file
+
 	socket = socket(AF_INET, SOCK_DGRAM)
-	socket.bind(('', port))
-	print("The server is ready to receive")
 
-	while 1:
-		data, addr = socket.recvfrom(2048)   # extracts sender IP and port number
-		stp_packet = pickle.loads(data)
-		print(data)
-		socket.sendto(data, addr)
+	def stp_rcv(self):
+		self.socket.bind(('', self.port))
+		data, addr = self.socket.recvfrom(2048)   # extracts sender IP and port number
+		stp_packet = pickle.loads(data)			  # converts data back to packet
+		#print(stp_packet)
+		return stp_packet
 
+	# FIN close
+	def stp_close(self):
+		self.socket.close()
 
-	#def stp_rcv(self, packet):
-	#	print("data received")
+	def parse_data(self, packet):
+		data = stp_packet.data
+		return data
 
+	def stp_append(self, data):
+		f = open("r_test.txt", "a+")
+		f.write(data)
+		f.close()
 
-	#def stp_rcv(self, packet)
-
+	def stp_send(self, packet):
+		# sender explicitly attaches IP destination address and port no. to each packet
+		self.socket.sendto(pickle.dumps(stp_packet), (self.r_host_ip, self.r_port))
+		#return_msg, server_add = socket.recvfrom(2048)	# receives data from server
+		self.socket.close()
 
 	# receive packet
 	#data, addr = socket.recvfrom(4096)
 	#stp_packet = pickle.loads(data)
 
-
-x = Receiver()
-#x.stp_rcv()
-
-
+num_args = 3
+if len(sys.argv) != num_args:				# check num args
+	print("Usage: ./Receiver.py port file.txt")
+else:
+	r_port, file = sys.argv[1:]				# grab args
+	receiver = Receiver(r_port, file)		# create instance of sender
+	print("Receiver is ready ...")
+	# waiting for file from sender
+	while True:
+		stp_packet = receiver.stp_rcv()  		# receive packet
+		data = receiver.parse_data(stp_packet)  # obtain payload from packet
+		receiver.stp_append(data)		 		# append packet data to r_file.txt
+		break
+	# everything finished -> close connection
+	# print test the output file
+	f = open("r_test.txt","r")
+	print(f.read())
+	receiver.stp_close()
 
 # after receiving packet
 #received_seq_num = stp_packet.seq_num
