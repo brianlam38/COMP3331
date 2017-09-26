e1249952aaf3d4c87ebdbf3f63626303239ed14c
brianlam38 <brian.lam38@gmail.com>
2017-09-26 14:30:33 +1000

    adding test topology and workload files

---
+++ Ass2/networks.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+class Graph:
+	# code stuff
+	def __init__ (self):
+		self.nodes = set()
+		self.edges = {}
+		self.weights = {}
+
+	def addNode(self, node):
+		self.nodes.add(node)
+		return None
+
+	def addEdge(self, from, to, weight):
+		self.edges = 
+		return None
+
+	def rmEdge(self):
+		return None
+
+	def rmNode(self):
+		return None
+
+	return None
+
+class Nodes:
+	# node stuff
+	return None
+
+class Edge:
+	# node stuff
+	return None
+
