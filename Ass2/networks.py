20c9b590bbcef9e453e4beeb584bee25c1c3660d
brianlam38 <brian.lam38@gmail.com>
2017-09-26 18:28:19 +1000

    adding routing perf program methods, main method

--- Ass2/networks.py
+++ Ass2/networks.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
 
+import sys
 from collections import *
 
-# 2D Matrix Rep
+"""
+2D Matrix Graph Representation
+"""
 class Graph:
 
-	def __init__(self, num_nodes):
-		self.num_nodes = 0
-		self.nodes = set()					# routers
+	def __init__(self):
+		#self.num_nodes = 0
+		self.nodes = set()					# switches/routers
 		self.edges = defaultdict(list)		# links
 		self.delays = {}					# link delays
-		self.cap = {}					# circuit capacity
+		self.cap = {}						# circuit capacity
 		self.load = {}						# load = Active Circuits / Capacity
 
+	# add a new switch/router
 	def addNode(self, node):
 		self.nodes.add(node)
 
+	# add a new link
 	# !neighbour = infinite delay
 	def addEdge(self, from_n, to_n, delay, capacity, active):
 		# init link
@@ -30,34 +35,63 @@ class Graph:
 		self.load[(from_n, to_n)] = active
 		self.load[(to_n, from_n)] = active
 
+"""
+Routing Performance Methods
+"""
+class RoutingPerf:
 
-class NetTopology:
+	def __init__(self, g, top, work):
+		self.graph = g
+		self.topology = top
+		self.workload = work
 
-	def __init__(self):
-		self.Graph = None
+	def initTopology(self):
+		f = open(self.topology, "r")
+		return None
 
-	def showGraph(self, graph):
-		for n in graph.nodes:
+	def initWorkload(self):
+		f = open(self.workload, "r")
+		return None
+
+	def showGraph(self):
+		for n in self.graph.nodes:
 			print("router = {}".format(n))
-			for e in graph.edges[n]:
+			for e in self.graph.edges[n]:
 				print("nb: {} | delay: {} | capacity: {} | load: {}" \
-				.format(e, graph.delays[(n, e)], graph.cap[(n, e)], graph.load[(n, e)]))
+				.format(e, self.graph.delays[(n, e)], self.graph.cap[(n, e)], self.graph.load[(n, e)]))
 			print("-----------------")
 		return None
 
+"""
+Main Function
+"""
 if __name__ == '__main__':
-	# init graph, nodes, edge and link values
-	g = Graph(4)
-	g.addNode('A')
-	g.addNode('B')
-	g.addNode('C')
-	g.addNode('D')
-	g.addEdge('A', 'B', 100, 10, 1)
-	g.addEdge('B', 'C', 200, 20, 2)
-	g.addEdge('C', 'D', 300, 30, 3)
+	num_args = 6
+	if len(sys.argv) != num_args:
+		print("Usage: ./networks.py NETWORK_SCHEME ROUTING_SCHEME TOPOLOGY_FILE WORKLOAD_FILE PACKET_RATE")
+	else:
+		graph = Graph()
+		# grab argument objects
+		NETWORK_SCHEME, ROUTING_SCHEME, TOPOLOGY_FILE, WORKLOAD_FILE, PACKET_RATE = sys.argv[1:]
+		r = RoutingPerf(graph, TOPOLOGY_FILE, WORKLOAD_FILE)
+		r.initTopology()
+		#r.initWorkload()
 
-	network = NetTopology()
-	network.showGraph(g)
+		# init graph, nodes, edge and link values
+		r.graph.addNode('A')
+		r.graph.addNode('B')
+		r.graph.addNode('C')
+		r.graph.addNode('D')
+		r.graph.addEdge('A', 'B', 100, 10, 1)
+		r.graph.addEdge('B', 'C', 200, 20, 2)
+		r.graph.addEdge('C', 'D', 300, 30, 3)
+		r.showGraph()
+
+
+
+
+
+
 
 
 
